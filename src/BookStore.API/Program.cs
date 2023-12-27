bcdb7aa2dbd678daec6b7feb19fe1ad805098a1c
Henrique S. Domareski <hsd.foz@gmail.com>
2023-12-27 20:01:22 +0100

    Refactored Program class (#4)
    * Deleted Startup class
    
    * Refactor Program class

--- src/BookStore.API/Program.cs
+++ src/BookStore.API/Program.cs
@@ -1,17 +1,47 @@
-namespace BookStore.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using BookStore.API.Configuration;
+using BookStore.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BookStoreDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.ResolveSwaggerConfig();
+
+builder.Services.AddCors();
+
+builder.Services.ResolveDependencies();
+
+var app = builder.Build();
+
+app.ConfigureSwagger();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var databaseSeeder = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
+        databaseSeeder.SeedData();
     }
 }
+
+app.Run();
