cbe842edc8fa1c4a17296f2b6cb4bf2fdbfd3a89
henriquesd <hsd.foz@gmail.com>
2023-12-27 19:07:37 +0100

    Deleted Startup class

--- src/BookStore.API/Program.cs
+++ src/BookStore.API/Program.cs
@@ -1,17 +1,74 @@
-namespace BookStore.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using BookStore.API.Configuration;
+using BookStore.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<BookStoreDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "BookStore API",
+        Version = "v1"
+    });
+});
+
+builder.Services.ConfigureSwaggerGen(options => options.ExampleFilters());
+builder.Services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+
+builder.Services.AddCors();
+
+builder.Services.ResolveDependencies();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var databaseSeeder = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
+        databaseSeeder.SeedData();
     }
 }
+
+app.Run();
