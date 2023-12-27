1b6a5cca82a6c83735985c2d134c5feaebe3df37
henriquesd <hsd.foz@gmail.com>
2023-12-27 19:58:12 +0100

    Refactor Program class

---
+++ src/BookStore.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Infrastructure.Context;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
+
+namespace BookStore.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection ResolveSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddScoped<BookStoreDbContext>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "BookStore API",
+                    Version = "v1"
+                });
+            });
+
+            services.ConfigureSwaggerGen(options => options.ExampleFilters());
+            services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+
+            return services;
+        }
+
+        public static WebApplication ConfigureSwagger(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
--- src/BookStore.API/Program.cs
+++ src/BookStore.API/Program.cs
@@ -1,13 +1,8 @@
 using BookStore.API.Configuration;
 using BookStore.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddDbContext<BookStoreDbContext>(options =>
 {
@@ -17,20 +12,10 @@ builder.Services.AddDbContext<BookStoreDbContext>(options =>
 builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo()
-    {
-        Title = "BookStore API",
-        Version = "v1"
-    });
-});
-
-builder.Services.ConfigureSwaggerGen(options => options.ExampleFilters());
-builder.Services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+builder.Services.ResolveSwaggerConfig();
 
 builder.Services.AddCors();
 
@@ -38,19 +23,7 @@ builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-});
+app.ConfigureSwagger();
 
 app.UseHttpsRedirection();
 
