0a1759b743b10e063187cb6146ff00862d6a928f
henriquesd <hsd.foz@gmail.com>
2023-12-27 18:29:16 +0100

    Removed unnecessary file

--- src/BookStore.API/Controllers/SwaggerExamples/ExampleConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookStore.API.Controllers.SwaggerExamples;
-
-public static class ExampleConstants
-{
-    public const string SuccessfulResponse = "Successful response";
-    public const string FailedResponse = "Failed response";
-}
